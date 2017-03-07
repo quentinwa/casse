d7a40b940977aef2223a7a5494f0c5890c57348f
quentinwa <quentinwa@laposte.net>
2017-03-07 10:04:21 +0100

    Texte sur entrée standard

--- changeCasse.cpp
+++ changeCasse.cpp
@@ -4,7 +4,7 @@
 #include <iostream>
 int main()
 {
-    std::string texte = "Toto, Tata";
+  std::string texte = "Toto, Tata";//c
     // change les minuscules par des majuscules et réciproquement
     for (char & c : texte)
     {
